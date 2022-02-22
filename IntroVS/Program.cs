fcc4f32f59486181d94b6c95fd55c199fdbfe60b
juan jesus ojeda castelo <joc028@ual.es>
2022-02-22 18:00:45 +0100

    New project created.

---
+++ IntroVS/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+
+namespace IntroVS
+{
+    public abstract class IntroAbs
+    {
+        public abstract string run();
+        public abstract void stop();
+    }
+    
+    public class Program:IntroAbs
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public override string run()
+        {     
+            throw new NotImplementedException();
+        }
+
+        public override void stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
